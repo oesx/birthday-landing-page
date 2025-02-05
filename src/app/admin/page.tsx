2a117e2868a568a976e8d2caa168108de6da53b4
oesx <oesxhello@gmail.com>
2025-02-05 20:02:33 +0800

    fix: migrate /messages to /admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,5 @@
-import { MessageManagement } from "@/components/message-management";
-import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-export default async function MessagesPage() {
-  const { userId } = auth();
-
-  if (!userId) {
-    redirect("/sign-in?redirect_url=/admin");
-  }
-
-  return <MessageManagement />;
+export default function MessagesPage() {
+  redirect("/admin");
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ export default authMiddleware({
     '/sign-up',
     '/api/webhooks/user',
     '/api/webhooks/clerk',
-    '/api/messages',  // 公开的留言板 API
+    '/api/admin',  // 公开的留言板 API
   ],
   ignoredRoutes: [
     '/((?!api|trpc))(_next|.+..+)(.*)'
